105477c42f950892ee66827bf6fa40873938e276
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-06-19 10:32:45 +0200

    feat(core/api/articles): optional sort type

--- src/minipress.core/src/Application/Actions/Article/ListArticlesAction.php
+++ src/minipress.core/src/Application/Actions/Article/ListArticlesAction.php
@@ -10,18 +10,30 @@ final class ListArticlesAction extends ArticleAction
 {
     protected function action(): Response
     {
-        $articles = $this->repository->findAll();
+        $params = $this->request->getQueryParams();
+        $articles = [];
+
+        if (isset($params['sort'])) {
+            $articles = match ($params['sort']) {
+                'date-asc' => $this->repository->orderBy('date_creation', 'ASC'),
+                'date-desc' => $this->repository->orderBy('date_creation', 'DESC'),
+                'auteur' => $this->repository->orderBy('auteur_id', 'ASC'),
+                default => $this->repository->findAll(),
+            };
+        } else {
+            $articles = $this->repository->findAll();
+        }
 
         $articles = array_map(function ($article) {
             return [
                 'title' => $article['titre'],
                 'creation_date' => $article['date_creation'],
                 'auteur_id' => $article['auteur_id'],
-                'url' => "TODO"
+                'url' => 'TODO'
             ];
         }, $articles);
 
-        $this->logger->info("Articles list was viewed.");
+        $this->logger->info('Articles list was viewed.');
 
         return $this->respondWithData($articles);
     }
