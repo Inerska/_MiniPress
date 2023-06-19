68065dc129d230564ac067071454ec5ed6e703c2
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-06-19 15:34:37 +0200

    feat(core/action): resolve dependency in all sub actions

--- src/minipress.core/src/Application/Actions/Category/CategoryAction.php
+++ src/minipress.core/src/Application/Actions/Category/CategoryAction.php
@@ -6,15 +6,16 @@ namespace App\Application\Actions\Category;
 
 use App\Application\Actions\Action;
 use App\Infrastructure\Persistence\Category\CategoryRepository;
+use Illuminate\View\Factory;
 use Psr\Log\LoggerInterface;
 
 abstract class CategoryAction extends Action
 {
     protected CategoryRepository $repository;
 
-    public function __construct(LoggerInterface $logger, CategoryRepository $repository)
+    public function __construct(LoggerInterface $logger, Factory $view, CategoryRepository $repository)
     {
-        parent::__construct($logger);
+        parent::__construct($logger, $view);
         $this->repository = $repository;
     }
 }
