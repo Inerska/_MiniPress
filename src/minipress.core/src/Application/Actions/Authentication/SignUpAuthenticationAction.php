11cf1695ba763cbfb4169901060f40bbc3056e97
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-06-20 14:50:39 +0200

    misc(core/auth): TODO: register server instance singleton to DI

--- src/minipress.core/src/Application/Actions/Authentication/SignUpAuthenticationAction.php
+++ src/minipress.core/src/Application/Actions/Authentication/SignUpAuthenticationAction.php
@@ -2,14 +2,25 @@
 
 declare(strict_types=1);
 
-namespace Minipress\Infrastructure\Persistence\Service\Identity;
+namespace Minipress\Application\Actions\Authentication;
 
-use Minipress\Application\Actions\Authentication\AuthenticationAction;
+use App\Domain\User\User;
 use Psr\Http\Message\ResponseInterface as Response;
 
 final class SignUpAuthenticationAction extends AuthenticationAction
 {
     protected function action(): Response
     {
+        $request = $this->request->getParsedBody();
+
+        $user = new User();
+        $user->name = $request['name'];
+        $user->email = $request['email'];
+        $user->password = password_hash($request['password'], PASSWORD_DEFAULT);
+        $user->save();
+
+        $this->service->user()->set($user);
+
+        return $this->response->withHeader('Location', '/admin');
     }
 }
