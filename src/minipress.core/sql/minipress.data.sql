05e3adfa0ed56e6e47f2345a6ddb147fe0541fa4
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-06-22 14:54:20 +0200

    feat(core): publish/unpublish works

--- src/minipress.core/sql/minipress.data.sql
+++ src/minipress.core/sql/minipress.data.sql
@@ -19,7 +19,7 @@ VALUES ('Sport'),
        ('Technologie')
 ON DUPLICATE KEY UPDATE nom = VALUES(nom);
 
-INSERT INTO articles (titre, resume, contenu, date_creation, auteur_id)
+INSERT INTO articles (titre, resume, contenu, date_creation, auteur_id, published)
 VALUES
     ('Article 1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
      '# Article 1\n
@@ -29,7 +29,7 @@ VALUES
      Proin non mauris lobortis, hendrerit ligula in, ullamcorper massa. Sed sit amet neque cursus, malesuada lorem et, lacinia elit.\n
      ## Paragraph 3\n
      Aliquam in nibh id lorem laoreet elementum id eget mi. Quisque in lectus felis.',
-     NOW(), 1),
+     NOW(), 1, 0),
 
     ('Article 2', 'Proin non mauris lobortis, hendrerit ligula in.',
      '# Article 2\n
@@ -39,7 +39,7 @@ VALUES
      Cras in sapien id nisi condimentum aliquet. Suspendisse potenti. Vestibulum consectetur orci non dui maximus, in elementum ligula egestas.\n
      ## Paragraph 3\n
      Duis in neque enim. Sed in ornare sem, ac posuere orci. Phasellus laoreet lacinia ex, non viverra turpis pulvinar non.',
-     NOW(), 2),
+     NOW(), 2, 1),
 
     ('Article 3', 'Donec aliquam, massa et imperdiet ornare.',
      '# Article 3\n
@@ -49,7 +49,7 @@ VALUES
      Phasellus volutpat, tellus eget maximus posuere, erat ipsum pharetra lacus, ut rutrum nulla lectus at augue.\n
      ## Paragraph 3\n
      Etiam cursus, sapien a eleifend gravida, dui purus tristique ipsum, id auctor quam orci non est.',
-     NOW(), 2),
+     NOW(), 2, 1),
 
     ('Article 4', 'Vestibulum consectetur orci non dui maximus.',
      '# Article 4\n
@@ -59,7 +59,7 @@ VALUES
      Vivamus cursus massa sit amet sem aliquet, non efficitur tortor interdum. Sed sem augue, auctor at efficitur id, posuere nec tellus.\n
      ## Paragraph 3\n
      Ut euismod tincidunt est, et porta lorem lobortis id. Integer varius, elit eget laoreet congue, diam nisl convallis diam, sit amet luctus mi neque non quam.',
-     NOW(), 1),
+     NOW(), 1, 0),
 
     ('Article 5', 'Curabitur at elit eu nunc consequat condimentum.',
      '# Article 5\n
@@ -69,7 +69,7 @@ VALUES
      Duis euismod, ligula et commodo dapibus, erat erat iaculis odio, at interdum quam est in lectus.\n
      ## Paragraph 3\n
      Fusce consequat, ante sed rutrum gravida, turpis nulla tempus ligula, id ultrices mi turpis eget lorem.',
-     NOW(), 2),
+     NOW(), 2, 1),
 
     ('Article 6', 'Duis in neque enim. Sed in ornare sem.',
      '# Article 6\n
@@ -79,7 +79,7 @@ VALUES
      In quis neque at orci porttitor cursus. Pellentesque volutpat, mi ut varius vehicula, ipsum urna pulvinar nunc, et commodo nisl orci non metus.\n
      ## Paragraph 3\n
      Quisque ullamcorper, felis sit amet egestas pulvinar, est ligula varius eros, non dignissim metus est sed metus.',
-     NOW(), 1),
+     NOW(), 1, 1),
 
     ('Article 7', 'Quisque ullamcorper, felis sit amet egestas pulvinar.',
      '# Article 7\n
@@ -89,7 +89,7 @@ VALUES
      Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse sed justo ac nibh gravida suscipit.\n
      ## Paragraph 3\n
      Etiam tincidunt, justo ac porttitor suscipit, est libero consequat nunc, non venenatis risus urna non libero.',
-     NOW(), 1),
+     NOW(), 1, 1),
 
     ('Article 8', 'Cras sollicitudin, justo nec pellentesque feugiat.',
      '# Article 8\n
@@ -99,7 +99,7 @@ VALUES
      Morbi at magna ac neque ultricies aliquet. Aenean ut enim dolor. Maecenas sed sem at dolor bibendum commodo.\n
      ## Paragraph 3\n
      Nunc sed arcu eget quam commodo bibendum. Integer at nisi sed diam faucibus posuere at nec mauris.',
-     NOW(), 2)
+     NOW(), 2, 1)
 ON DUPLICATE KEY UPDATE titre = VALUES(titre), resume = VALUES(resume), contenu = VALUES(contenu), date_creation = VALUES(date_creation), auteur_id = VALUES(auteur_id);
 
 INSERT INTO article_categories (article_id, category_id)
