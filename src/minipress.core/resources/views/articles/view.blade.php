b9601a569f87408879d453c20c9cead91cc17d76
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-06-22 11:55:12 +0200

    feat(core): display only if published

--- src/minipress.core/resources/views/articles/view.blade.php
+++ src/minipress.core/resources/views/articles/view.blade.php
@@ -56,6 +56,14 @@
                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                         Date de création
                     </th>
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Status
+                    </th>
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Action
+                    </th>
                 </tr>
                 </thead>
                 <tbody id="tableBody" class="bg-white divide-y divide-gray-200">
@@ -71,7 +79,7 @@
         $(document).ready(function () {
             const table = $('#myTable').DataTable({
                 ajax: {
-                    url: '/api/v1/articles',
+                    url: '/api/v1/articles?display=all',
                     dataSrc: 'data',
                     beforeSend: function () {
                         $('#loading').show();
@@ -82,11 +90,50 @@
                 },
                 columns: [
                     {data: 'titre'},
-                    {data: 'date_creation'}
+                    {data: 'date_creation'},
+                    {
+                        data: 'published',
+                        render: function (data, type, row) {
+                            return data ? 'Publié' : 'Non publié';
+                        }
+                    },
+                    {
+                        data: null,
+                        render: function (data, type, row) {
+                            const id = row.url.split('/')[2];
+                            if (row.published) {
+                                return `<button class="unpublish-button bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" data-id="${id}">Unpublish</button>`;
+                            } else {
+                                return `<button class="publish-button bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" data-id="${id}">Publish</button>`;
+                            }
+                        }
+                    },
                 ],
                 paging: false,
                 searching: false,
                 info: false,
+            });
+
+            $('#myTable').on('click', '.publish-button', function () {
+                const id = $(this).data('id');
+                $.ajax({
+                    url: `/api/v1/articles/${id}/publish`,
+                    type: 'PUT',
+                    success: function () {
+                        table.ajax.reload();
+                    }
+                });
+            });
+
+            $('#myTable').on('click', '.unpublish-button', function () {
+                const id = $(this).data('id');
+                $.ajax({
+                    url: `/api/v1/articles/${id}/unpublish`,
+                    type: 'PUT',
+                    success: function () {
+                        table.ajax.reload();
+                    }
+                });
             });
 
             $('#categoryFilter').on('change', function() {
