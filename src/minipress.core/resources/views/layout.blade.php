291f1a2b3b2d6a8915b4f683b879d69ca800cac9
Alexis C. Gridel <contact@agdl.dev>
2023-06-22 09:10:19 +0200

    style(core/admin): rename title admin panel

--- src/minipress.core/resources/views/layout.blade.php
+++ src/minipress.core/resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Panel</title>
+    <title>Panel d'administration</title>
     <link href="//cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css"/>
 
     <script src="//code.jquery.com/jquery-3.7.0.min.js"
